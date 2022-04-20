d39ca6b536d2302d9caaec1b6f6066ec57710d8e
Matteo <Matteo@ASUS-7812A>
2022-04-20 12:16:35 +0200

    lab05

--- src/main/java/it/polito/tdp/anagrammi/EntryPoint.java
+++ src/main/java/it/polito/tdp/anagrammi/EntryPoint.java
@@ -2,6 +2,8 @@ package it.polito.tdp.anagrammi;
 
 import javafx.application.Application;
 import static javafx.application.Application.launch;
+
+import it.polito.tdp.anagrammi.model.AnagrammiModel;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -12,10 +14,13 @@ public class EntryPoint extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Scene.fxml"));
-        
+    	FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Scene.fxml"));
+    	Parent root = loader.load();
         Scene scene = new Scene(root);
-        scene.getStylesheets().add("/styles/Styles.css");
+        
+        AnagrammiModel model = new AnagrammiModel();
+        FXMLController controller = loader.getController();
+        controller.setModel(model);
         scene.getRoot().setStyle("-fx-font-family: 'serif'");
         
         stage.setTitle("JavaFX and Maven");
--- src/main/java/it/polito/tdp/anagrammi/FXMLController.java
+++ src/main/java/it/polito/tdp/anagrammi/FXMLController.java
@@ -1,6 +1,86 @@
 package it.polito.tdp.anagrammi;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.Set;
+
+import it.polito.tdp.anagrammi.model.AnagrammiModel;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class FXMLController {
+	
+	private AnagrammiModel model;
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button btnCalcola;
+
+    @FXML
+    private Button btnReset;
+
+    @FXML
+    private TextArea txtAnagrammiCorr;
+
+    @FXML
+    private TextArea txtAnagrammiErr;
+
+    @FXML
+    private TextField txtPaolaIniziale;
+
+    @FXML
+    void handleCalcola(ActionEvent event) {
+    	String parola = txtPaolaIniziale.getText();
+    	txtAnagrammiCorr.clear();
+    	txtAnagrammiErr.clear();
+    	
+    	if(parola.length()<2) {
+    		txtAnagrammiCorr.appendText("La parola inserita è troppo corta");
+    	}
+    	if(parola.length()>8) {
+    		txtAnagrammiCorr.appendText("La parola inserita è troppo lunga");
+    	}
+    	
+    	Set<String>anagrammi= this.model.calcolaAnagrammi(parola);
+    	for(String anagramma : anagrammi) {
+    		if(this.model.isCorrect(anagramma)) {
+    			txtAnagrammiCorr.appendText(anagramma +"\n");
+    		}
+    		else {
+    			txtAnagrammiErr.appendText(anagramma +"\n");
+    		}
+    	}
+
+    }
+
+    @FXML
+    void handleReset(ActionEvent event) {
+    	txtAnagrammiCorr.clear();
+    	txtAnagrammiErr.clear();
+    	txtPaolaIniziale.clear();
+
+    }
+
+    @FXML
+    void initialize() {
+        assert btnCalcola != null : "fx:id=\"btnCalcola\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtAnagrammiCorr != null : "fx:id=\"txtAnagrammiCorr\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtAnagrammiErr != null : "fx:id=\"txtAnagrammiErr\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtPaolaIniziale != null : "fx:id=\"txtPaolaIniziale\" was not injected: check your FXML file 'Scene.fxml'.";
+
+    }
+    
+    public void setModel(AnagrammiModel model) {
+    	this.model = model;
+    }
 
 }
